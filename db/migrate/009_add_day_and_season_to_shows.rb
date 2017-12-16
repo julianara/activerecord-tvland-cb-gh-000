2505c80c23b6c57dd5c47cbe820b92a93648233b
Julia Nara <julianara888@gmail.com>
2017-12-16 03:55:28 +0000

    automatically backed up by learn

--- db/migrate/009_add_day_and_season_to_shows.rb
+++ db/migrate/009_add_day_and_season_to_shows.rb
@@ -1,4 +1,4 @@
-class AddDayAndSeasonToShows < ActionRecord::Migration 
+class AddDayAndSeasonToShows < ActiveRecord::Migration 
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
